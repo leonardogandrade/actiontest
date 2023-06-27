0b1bba1512d2d7e87229dd0a1045a0ed81c70c40
leonardogandrade <leonardo.g.andrade@gmail.com>
2023-06-27 17:19:41 -0300

    env

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 function connect() {
     try {
-        mongoose.connect(process.env.MONGO_DB.toString());
+        mongoose.connect("mongodb+srv://admin:admin@cluster0.czex64t.mongodb.net/actionsdb?retryWrites=true&w=majority");
         console.log('mongodb connected successfully.')
     } catch (err) {
         console.log(`Error while connectiong to mongodb. -  ${err}.`)
