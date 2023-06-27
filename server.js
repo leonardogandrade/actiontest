430c1cddcd76941888fab1045a0c5fd2db33f111
leonardogandrade <leonardo.g.andrade@gmail.com>
2023-06-27 18:43:49 -0300

    env

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const server = express();
 const requireDir = require('require-dir');
 requireDir('./src/models')
-// require('dotenv').config();
 const db = require('./src/database/connection')
 const routes = require('./src/routes');
 const PORT = 3005;
