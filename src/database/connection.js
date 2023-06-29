263d479a5e8ef9b8733432387892a35905e54778
leonardogandrade <leonardo.g.andrade@gmail.com>
2023-06-29 15:21:09 -0300

    push to aws ECR 5

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 function connect() {
     try {
         mongoose.connect("mongodb+srv://admin:admin@cluster0.czex64t.mongodb.net/actionsdb?retryWrites=true&w=majority");
-        console.log('mongodb connected successfully.')
+        console.log('mongodb connected successfully!')
     } catch (err) {
         console.log(`Error while connectiong to mongodb. -  ${err}.`)
     }
